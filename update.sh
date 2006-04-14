98f014a584a8cdf3c0aedc4e4264fc0bc4496710
shish <shish@7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db>
2006-04-14 21:39:41 +0000

    deal with noexec
    git-svn-id: file:///home/shish/svn/travmap/trunk@189 7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db

--- update.sh
+++ update.sh
@@ -11,39 +11,39 @@ BASEDIR=/home/www/shish.is-a-geek.net/htdocs/projects/travmap
 cd $BASEDIR
 
 function foreach() {
-	$1 s1.travian.com map
-	$1 s2.travian.com map
-	$1 s3.travian.com map
-	$1 s4.travian.com map
-	$1 s5.travian.com map
+	sh $1 s1.travian.com map
+	sh $1 s2.travian.com map
+	sh $1 s3.travian.com map
+	sh $1 s4.travian.com map
+	sh $1 s5.travian.com map
 
-	$1 s1.travian.fr map
-	$1 s2.travian.fr map
-	$1 s3.travian.fr map
-	$1 s4.travian.fr map
-	$1 s5.travian.fr map
-	$1 s6.travian.fr map
-	$1 s7.travian.fr map
-	$1 s8.travian.fr map
+	sh $1 s1.travian.fr map
+	sh $1 s2.travian.fr map
+	sh $1 s3.travian.fr map
+	sh $1 s4.travian.fr map
+	sh $1 s5.travian.fr map
+	sh $1 s6.travian.fr map
+	sh $1 s7.travian.fr map
+	sh $1 s8.travian.fr map
 
-	$1 s1.travian.it map
-	$1 s2.travian.it map
-	$1 s3.travian.it map
-	$1 s4.travian.it map
-	$1 s5.travian.it map
+	sh $1 s1.travian.it map
+	sh $1 s2.travian.it map
+	sh $1 s3.travian.it map
+	sh $1 s4.travian.it map
+	sh $1 s5.travian.it map
 
-	$1 www.travian.org karte
-	$1 www.travian.at  karte
+	sh $1 www.travian.org karte
+	sh $1 www.travian.at  karte
 
-	$1 s1.travian.net map
-	$1 s2.travian.net map
+	sh $1 s1.travian.net map
+	sh $1 s2.travian.net map
 
-	$1 s1.travian.nl map
-	$1 s2.travian.nl map
+	sh $1 s1.travian.nl map
+	sh $1 s2.travian.nl map
 
-	$1 welt1.travian.de karte
-	$1 welt2.travian.de karte
-	$1 welt3.travian.de karte
+	sh $1 welt1.travian.de karte
+	sh $1 welt2.travian.de karte
+	sh $1 welt3.travian.de karte
 }
 
 
