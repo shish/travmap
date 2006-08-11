b693f57792d743d3182010c1a0befaff9bd2ddce
shish <shish@7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db>
2006-08-11 18:43:59 +0000

    missed a spot -- the database connection needs to be told to use utf8 too
    git-svn-id: file:///home/shish/svn/travmap/trunk@320 7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db

--- database.php
+++ database.php
@@ -26,6 +26,7 @@ else {
 	require_once "config.php";
 	mysql_pconnect($mysql_host, $mysql_user, $mysql_pass);
 	mysql_select_db($mysql_db);
+	mysql_query("SET NAMES utf8");
 	function sql_fetch_row($result) {return mysql_fetch_assoc($result);}
 	function sql_escape_string($text) {return mysql_escape_string($text);}
 	function sql_query($query) {
