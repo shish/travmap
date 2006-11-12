09bc5e63892c424a50a1db4b769f419fb25ba0b5
shish <shish@7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db>
2006-11-12 09:04:41 +0000

    minor verbosity
    git-svn-id: file:///home/shish/svn/travmap/trunk@335 7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db

--- update_text.sh
+++ update_text.sh
@@ -8,18 +8,21 @@
 
 DBNAME=`echo $1 | sed 's/\./_/g'`
 
+echo -n  "Downloading http://$1/$2.sql... "
+
 # if the SQL file is less than 4 hours old, leave it
 if [ -f sql/$1.sql ] ; then
 	NOW=`date +"%s"`
 	THEN=`stat -L -c %Y sql/$1.sql`
 	DIFF=`expr $NOW - $THEN`
 	if [ $DIFF -lt 43200 ] ; then # 43200 sec = 12 hour
+		echo "cached"
 		exit
 	fi
 fi
 
-echo -n  "Downloading http://$1/$2.sql... "
 if wget -q http://$1/$2.sql -O sql/$1.sql ; then
+	touch sql/$1.sql
 	echo "ok"
 else
 	echo "failed"
