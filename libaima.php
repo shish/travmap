425395bf669b60e4ed38370b7299797972bd0847
shish <shish@7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db>
2007-01-02 22:15:23 +0000

    credit a url which exists
    git-svn-id: file:///home/shish/svn/travmap/trunk@351 7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db

--- libaima.php
+++ libaima.php
@@ -25,7 +25,7 @@ function aimacreate($w, $h) {
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
   "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<!-- Created with Aima (http://shish.is-a-geek.net/) -->
+<!-- Created with Aima (http://trac.shishnet.org/phplibs/) -->
 <svg width="$w" height="$h" version="1.1"
 	xmlns="http://www.w3.org/2000/svg"
 	xmlns:xlink="http://www.w3.org/1999/xlink"
