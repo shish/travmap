136443b8311762b37a3391475217849fd0b67356
Shish <shish@shishnet.org>
2009-03-19 09:00:17 +0000

    order by number, not name

--- htdocs/ajax.php
+++ htdocs/ajax.php
@@ -2,7 +2,7 @@
 require_once "database.php";
 if($_GET["mode"] == "servers") {
 	$country = sql_escape_string($_GET["country"]);
-	$result = sql_query("SELECT * FROM servers WHERE country='$country' AND visible=True ORDER BY name");
+	$result = sql_query("SELECT * FROM servers WHERE country='$country' AND visible=True ORDER BY num");
 	while($row = sql_fetch_row($result)) {
 		print $row["name"] . "," . ($row["population"] > 1000) . "\n";
 	}
