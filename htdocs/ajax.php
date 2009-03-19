d79b92ce5348ab68b98c79de2ddba3442ed5eb22
shish <shish@7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db>
2009-03-19 08:16:32 +0000

    oh yeah, this was used...
    git-svn-id: file:///home/shish/svn/travmap/trunk@442 7c5a17a3-5d09-0410-ab8d-b6f3ef2aa8db

---
+++ htdocs/ajax.php
@@ -0,0 +1,10 @@
+<?php
+require_once "database.php";
+if($_GET["mode"] == "servers") {
+	$country = sql_escape_string($_GET["country"]);
+	$result = sql_query("SELECT * FROM servers WHERE country='$country' AND visible=True ORDER BY name");
+	while($row = sql_fetch_row($result)) {
+		print $row["name"] . "," . ($row["population"] > 1000) . "\n";
+	}
+}
+?>
