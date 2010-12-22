bc7308b1e91680d802b37490ed8fbdd475a99b22
Shish <shish@shishnet.org>
2010-12-22 19:38:24 +0000

    GET variables, for easier logging

--- htdocs/add.php
+++ htdocs/add.php
@@ -4,9 +4,9 @@ if(
 	preg_match("/^[a-z0-9\.]+$/", $_GET["server"]) &&
 	preg_match("/^[A-Za-z ]+$/", $_GET["country"])
 ) {
-	$server = $_POST["server"];
-	$country = $_POST["country"];
-	$num = time();
+	$server = $_GET["server"];
+	$country = $_GET["country"];
+	$num = (int)time();
 	print "adding $country / $server\n";
 	chdir("/home/shish/travmap.shishnet.org/utils/");
 	system("./add_server $server '$country' $num");
