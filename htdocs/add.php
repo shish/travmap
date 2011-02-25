ba1177469f104a50b235e286fccd201c0d1bfcc2
Shish <shish@shishnet.org>
2011-02-25 14:11:46 +0000

    People are really, really dumb :/

--- htdocs/add.php
+++ htdocs/add.php
@@ -1,15 +1,21 @@
 <pre>
 <?php
-if(
-	preg_match("/^[a-z0-9\.]+$/", $_GET["server"]) &&
-	preg_match("/^[A-Za-z ]+$/", $_GET["country"])
-) {
-	$server = $_GET["server"];
-	$country = $_GET["country"];
-	$num = (int)time();
-	print "adding $country / $server\n";
-	chdir("/home/shish/travmap.shishnet.org/utils/");
-	system("./add_server $server '$country' $num");
+if(!preg_match("/^[A-Z][A-Za-z ]+$/", $_GET["country"])) {
+	die("Invalid country name");
 }
+
+if(!(
+	preg_match("/^[a-z0-9\.]+$/", $_GET["server"]) &&
+	dns_get_record($_GET["server"])
+)) {
+	die("Invalid server name");
+}
+
+$server = $_GET["server"];
+$country = $_GET["country"];
+$num = (int)time();
+print "adding $country / $server\n";
+chdir("/home/shish/travmap.shishnet.org/utils/");
+system("./add_server $server '$country' $num");
 ?>
 </pre>
