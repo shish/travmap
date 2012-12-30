bae01f23b6da4fb7dd3e7f3328e94d1ee8666606
Shish <shish@shishnet.org>
2012-12-30 16:56:22 +0000

    ignore caching

--- utils/clear_cache.sh
+++ utils/clear_cache.sh
@@ -3,19 +3,6 @@
 cd `dirname $0`
 . config.sh
 
-start=`date +%l:%M%p`
-
 rm -f $CACHE/*.txt
-count=0
-for n in `ls -t $CACHE` ; do
-	count=$[$count+1]
-	percent=$[$count*100/256]
-	dir=`basename $n`
-	echo -n "Cleaning cache $percent% (started $start)" > $STATUS
-	for o in $CACHE/$n/* ; do
-		rm -f $o/*
-	done
-	sleep 1
-done
 
 echo -n > $STATUS
--- utils/update.sh
+++ utils/update.sh
@@ -13,17 +13,6 @@ cd `dirname $0`
 if [ ! -d ../sql ] ; then mkdir ../sql ; fi
 if [ ! -d $CACHE ] ; then
 	mkdir $CACHE
-	zeroTo255=`seq -s " " -f %g 0 255`
-	zeroToFF=`for n in $zeroTo255 ; do printf "%2.2x " $n ; done`
-	for m in $zeroToFF ; do 
-		mkdir $CACHE/$m
-		echo -n "$m: "
-		for n in $zeroToFF ; do
-			echo -n .
-			mkdir $CACHE/$m/$n
-		done
-		echo
-	done
 	chmod -R 777 $CACHE
 fi
 
@@ -32,6 +21,5 @@ fi
 ./pgsql.sh -t -A -F " " -c "SELECT name FROM servers WHERE visible=True ORDER BY country, num" | xargs -l1 ./update_pgsql.sh
 
 ./clear_cache.sh
-./clear_dbcache.sh
 
 echo "Update complete at `date +%l:%M%p`" > $STATUS
