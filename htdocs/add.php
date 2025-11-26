706159041120dde9e04ccb65badfff5a803b95ce
Shish <shish@shishnet.org>
2025-11-26 18:00:26 +0000

    remove country column, just use server name

--- htdocs/add.php
+++ htdocs/add.php
@@ -1,9 +1,5 @@
 <pre>
 <?php
-if(!preg_match("/^[A-Z][A-Za-z ]+$/", $_GET["country"])) {
-	die("Invalid country name");
-}
-
 if(!preg_match("/^[a-z0-9\.\-]+$/", $_GET["server"])) {
 	die("Invalid server name");
 }
@@ -15,11 +11,11 @@ if(!dns_get_record($_GET["server"])) {
 */
 
 $server = $_GET["server"];
-$country = $_GET["country"];
 $num = (int)time();
 $mapfile = strpos($server, "-") ? "json" : "map";
-print "adding $country / $server / $mapfile\n";
+print "adding $server / $mapfile\n";
 chdir("/utils/");
-system("./add_server $server '$country' $num $mapfile 2>&1");
+system("./add_server $server $num $mapfile 2>&1");
 ?>
 </pre>
+
