bd79c63f7d3b495b7434aaf112bd72b7a7692fd7
Shish <shish@shishnet.org>
2025-11-28 09:23:47 +0000

    derp

--- htdocs/lib/database.php
+++ htdocs/lib/database.php
@@ -5,8 +5,6 @@
  * open the database
  */
 
-require_once "config.php";
-
 $db = new \PDO("sqlite:../data/travmap.sqlite");
 if(!$db) throw new Exception("Failed to connect to database");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
