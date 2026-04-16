372926744fc5aacb3614e607070d0ea728a1e51c
Shish <shish@shishnet.org>
2026-04-16 22:37:50 +0100

    fix hypothetical path traversal

--- htdocs/lib/localise.php
+++ htdocs/lib/localise.php
@@ -5,9 +5,16 @@
  * load localisations from name=value file
  */
 
+function valid_lang(string $lang): bool {
+    if(strlen($lang) != 2 && strlen($lang) != 5) return false;
+    if(!preg_match('/^[a-z]{2}(-[A-Z]{2})?$/', $lang)) return false;
+    return file_exists("./lang/$lang.txt");
+}
+
 function get_lang() {
 	$lang = null;
 
+	// allow ?lang=xx to override browser settings, but only if the file exists
 	if(isset($_GET["lang"])) {
 		switch($_GET["lang"]) {
 			case 'cn': $glang = 'zh'; break;
@@ -16,22 +23,24 @@ function get_lang() {
 			case 'se': $glang = 'sv'; break;
 			default:   $glang = $_GET["lang"]; break;
 		}
-		if((strlen($glang) == 2 || strlen($glang) == 5) && (file_exists("./lang/$glang.txt"))) {
+		if(valid_lang($glang)) {
 			$lang = $glang;
 		}
 	}
 
+	// if not, try the browser settings
 	if(is_null($lang) && isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
 		$al = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 		$options = explode(',', str_replace(';', ',', $al));
 		foreach($options as $option) {
-			if(file_exists("./lang/$option.txt")) {
+			if(valid_lang($option)) {
 				$lang = $option;
 				break;
 			}
 		}
 	}
 
+	// default to english
 	if(is_null($lang)) {
 		$lang = "en";
 	}
