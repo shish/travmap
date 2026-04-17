fc54f6f1d7efd524b1f93c03ec4fac50f0c69d48
Shish <shish@shishnet.org>
2026-04-17 13:45:56 +0100

    strict types for database

--- htdocs/lib/database.php
+++ htdocs/lib/database.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 /*
  * database.php (c) Shish 2006
  *
@@ -8,3 +10,4 @@
 $db = new \PDO("sqlite:../data/travmap.sqlite");
 if(!$db) throw new Exception("Failed to connect to database");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
