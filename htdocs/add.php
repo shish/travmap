c2e0dd038a7317f60fa2c923a109a9110e0acf60
Shish <shish@shishnet.org>
2026-04-17 13:10:48 +0100

    strict types for add.php

--- htdocs/add.php
+++ htdocs/add.php
@@ -1,13 +1,15 @@
-<pre>
 <?php
-if(!preg_match("/^[a-z][a-z0-9\.\-]+$/", $_GET["server"])) {
-    http_response_code(400);
+declare(strict_types=1);
+
+header('Content-Type: text/plain; charset=utf-8');
+
+$server = $_GET["server"] ?? "";
+
+if (!preg_match("/^[a-z][a-z0-9\.\-]+$/", $server)) {
+	http_response_code(400);
 	die("Invalid server name");
 }
 
-$server = $_GET["server"];
 print "adding $server\n";
 chdir("/utils/");
 system("./manage.py add " . escapeshellarg($server) . " 2>&1");
-?>
-</pre>
